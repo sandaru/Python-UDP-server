1eeb23f3468970116bc6048c10e707e84d326201
Pushpitha Sandaruwan <sandaru.weerasooriya@gmail.com>
2014-06-23 17:02:41 +0530

    Function to auto detect the free ports
    Added a function to auto detect the  free ports. it starts from 0 and
    bind to the server just after find a free port

--- client.py
+++ client.py
@@ -1,11 +1,11 @@
 import socket
 
 print "Client 1"
-client_socket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)    #This creates socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)#This creates socket
 
 while 1:
     data=raw_input("Message:")
-    client_socket.sendto(data, ('fe80::8f6:5e7d:833:fea6%14',116))
+    client_socket.sendto(data, ('127.0.0.1',6))
     print "Sending request"
     recv_data, addr = client_socket.recvfrom(256)
     print "Message<<Clent2>>",recv_data
--- server.py
+++ server.py
@@ -1,19 +1,47 @@
 import socket
 
+addressBar = list()#list of active users
+#function to get free port from the host
+def get_free_port(socket_in_use):
+    initial_port_number = 1
+    success = False
+    while(not success and initial_port_number < 10):
+        try:
+            socket_in_use.bind(('0.0.0.0',initial_port_number))
+            success = True
+            #socket_in_use.close()
+        except Exception:
+            success = False
+            initial_port_number=initial_port_number+1
+    return initial_port_number
 
-addressBar = list();
-server_socket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)   
-server_socket.bind(('fe80::8f6:5e7d:833:fea6%14', 116))              
-print "UDPServer Waiting for client on port 900"
+#function to initial handshake
+def initiate(address):
+    addressBar.append(address)
+    #request related data from the database
+    #match and send the data
 
-while True:
-    dataFromClient, address = server_socket.recvfrom(256)
-    if(address not in addressBar):
-                addressBar.append(address)
-    print dataFromClient
-    if(len(addressBar)>1):
-                for add in addressBar:
-                        if(address != add ):
-                                server_socket.sendto(dataFromClient, add)
-    print addressBar
-    
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+port = int(get_free_port(server_socket))
+try:
+    print "UDPServer Waiting for client on port ",port
+    while True:
+        dataFromClient, address = server_socket.recvfrom(256)
+        if(address not in addressBar):
+                    initiate(address)
+        print dataFromClient
+        if(len(addressBar)>1):
+                    for add in addressBar:
+                            if(address != add ):
+                                    server_socket.sendto(dataFromClient, add)
+        else:
+            server_socket.sendto("No any connected devices", address)
+        print addressBar
+
+except Exception, e:
+    print "Port in use Error: ", e
+
+
+
+
